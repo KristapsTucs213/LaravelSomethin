0a8d591233ea3bcd4b57e0a16a4190a2897e3035
unknown <you@example.com>
2024-10-23 13:14:09 +0300

    Added create

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -3,11 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Note;
 
 class NoteController extends Controller
 {
     public function index()
     {
-        return "All Notes";
+        $notes = Note::all();
+        return view('notes.index', ['allNotes' => $notes]);
+    
+    }
+
+    public function create()
+    {
+        return view('notes.create');
+    }
+
+    public function store(Request $requst){
+        \Log::debug($requst);
+        $data = [
+            "title" => $requst -> title,
+            'content' => $requst ->content
+        ];
+
+        Note::create($data);
+
+        // return view('notes.index');
+        return redirect('/notes/');
     }
 }
